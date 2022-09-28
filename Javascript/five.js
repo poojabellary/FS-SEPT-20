13f57041fec3820e5fe57b20749eb5d35ad3f831
poojabellary <poojabellary714@gmail.com>
2022-09-28 12:40:42 +0530

    ok

---
+++ Javascript/five.js
@@ -0,0 +1,11 @@
+let a=100;
+let b=200;
+let c=1;
+console.log(a>b);
+console.log(a<c);
+console.log(a<=b);
+console.log(a>=c);
+console.log(a==b);
+console.log(a===b);
+
+
---
+++ Javascript/four.js
@@ -0,0 +1,7 @@
+let a=10;
+let b="20";
+let c=true
+console.log(a+b);
+console.log(a+c);
+console.log(a*b);
+console.log(a*c);
