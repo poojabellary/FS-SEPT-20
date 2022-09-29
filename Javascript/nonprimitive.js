f697fde090c28ee5cb1b637fbeea082a53b49a40
poojabellary <poojabellary714@gmail.com>
2022-09-29 11:45:36 +0530

    okkkk

---
+++ Javascript/nonprimitive.js
@@ -0,0 +1,4 @@
+var a=[];
+var b=[10,20,30]
+var size=['S','M','L','XL','S','L',10,20,"hello"];
+console.log(typeof size)
---
+++ Javascript/primitive.js
@@ -0,0 +1,8 @@
+var price=436;
+var rating=4.5;
+var name="slim fit";
+var discount=true;
+var brand="STONEBERG"
+console.log(brand)
+console.log(discount)
+console.log(name)
